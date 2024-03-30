4f8b24a657d9bd09b609bb3074e8f5fdfd510750
dinildamsith <dinildamsith52@gmail.com>
2024-03-30 22:01:30 +0530

    jwt Auth response class added

---
+++ src/main/java/lk/ijse/SpringBootDemo/reqAndResp/response/JwtAuthResponse.java
@@ -0,0 +1,14 @@
+package lk.ijse.SpringBootDemo.reqAndResp.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JwtAuthResponse {
+    private String token;
+}
