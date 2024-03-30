8c6cf718da12a5db04a16ac82678ada2037bfaa0
dinildamsith <dinildamsith52@gmail.com>
2024-03-30 22:02:45 +0530

    jwt service class added

---
+++ src/main/java/lk/ijse/SpringBootDemo/service/AuthenticationService.java
@@ -0,0 +1,12 @@
+package lk.ijse.SpringBootDemo.service;
+
+import lk.ijse.SpringBootDemo.reqAndResp.response.JwtAuthResponse;
+import lk.ijse.SpringBootDemo.reqAndResp.secure.SignIn;
+import lk.ijse.SpringBootDemo.reqAndResp.secure.SignUp;
+import org.springframework.stereotype.Service;
+
+
+public interface AuthenticationService {
+    JwtAuthResponse signIn(SignIn signIn);
+    JwtAuthResponse signUp(SignUp signUp);
+}
